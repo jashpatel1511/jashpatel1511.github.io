36fd190709a800af6bb602007b619c7c3b6fa012
JASH-PATEL-6566 <pateljash1511.com>
2025-02-11 13:59:51 -0400

    changes in routes

--- src/index.js
+++ src/index.js
@@ -1,50 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
 
 // pages
-import Root from './Pages/Root';
-import About from './Pages/About';
-import Contact from './Pages/Contact';
-import Services from './Pages/Services';
-import Resume from './Pages/Resume';
-import Portfolio from './Pages/Portfolio';
+import Root from "./Pages/Root";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
+import Services from "./Pages/Services";
+import Resume from "./Pages/Resume";
+import Portfolio from "./Pages/Portfolio";
 
-// routes
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/experience",
-    element: <Resume />,
-  },
-  {
-    path: "/portfolio",
-    element: <Portfolio />,
-  },
-  {
-    path: "/qualification",
-    element: <Services />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-  }
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Root />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/experience" element={<Resume />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/qualification" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
