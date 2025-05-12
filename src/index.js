986b299191e09e9052f71b51a1832fcf212e06f2
JASH-PATEL-6566 <pateljash1511.com>
2025-05-12 01:36:41 -0300

    fix issue related to routes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import "./index.css";
+
+// // pages
+// import Root from "./Pages/Root";
+// import About from "./Pages/About";
+// import Contact from "./Pages/Contact";
+// import Services from "./Pages/Services";
+// import Resume from "./Pages/Resume";
+// import Portfolio from "./Pages/Portfolio";
+// import ScrollTop from "./Components/ScrollTop";
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/">
+//       <ScrollTop />
+//       <Routes>
+//         <Route path="/" element={<Root />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="/experience" element={<Resume />} />
+//         <Route path="/project" element={<Portfolio />} />
+//         <Route path="/qualification" element={<Services />} />
+//         <Route path="/contact" element={<Contact />} />
+//       </Routes>
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 // pages
@@ -15,7 +45,7 @@ import ScrollTop from "./Components/ScrollTop";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <HashRouter>
       <ScrollTop />
       <Routes>
         <Route path="/" element={<Root />} />
@@ -25,6 +55,6 @@ root.render(
         <Route path="/qualification" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
